ddeafd4883496feb75864e49739ba0f29773b9b0
StephenDunne97 <dunne134@gmail.com>
2021-09-09 20:03:13 +0100

    Initial
    Pushing files that were created prior to rep creation.

---
+++ 3_jsx_variable_example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (
+  <ul>
+    <li> Item 1 </li>
+    <li> Item 2 </li>
+    <li> Item 3 </li>
+  </ul>
+);
+
+ReactDOM.render(myList, document.getElementById('app'));
